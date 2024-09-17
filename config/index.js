ec57f5bea7e4ccc59f17b9d5391fc8e6fb3e9d63
Mr-ayush25 <Mr.ayush1125@gmail.com>
2024-09-17 13:27:35 +0530

    updated vercel configs

--- config/index.js
+++ config/index.js
@@ -2,9 +2,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-    EXPRESS_PORT: process.env.EXPRESSPORT || 5001,
+    EXPRESS_PORT: process.env.EXPRESSPORT || 3000,
     MONGO_URI: process.env.MONGO_URI,
-    BASE_URL: `http://localhost:${process.env.EXPRESSPORT || 5001}`,
+    BASE_URL: `http://localhost:${process.env.EXPRESSPORT || 3000}`,
     NEWS_API_KEY: process.env.NEWS_API_KEY,
     ORIGIN: ["https://fakenews-eta.vercel.app/"],
 };
