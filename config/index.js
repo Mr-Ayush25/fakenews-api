82bc0c8a2bdf489c33fd3085197c9bc7767bff9f
Mr-ayush25 <Mr.ayush1125@gmail.com>
2024-09-17 13:50:43 +0530

    updated cors config

--- config/index.js
+++ config/index.js
@@ -6,5 +6,5 @@ export const config = {
     MONGO_URI: process.env.MONGO_URI,
     BASE_URL: `http://localhost:${process.env.EXPRESSPORT || 3000}`,
     NEWS_API_KEY: process.env.NEWS_API_KEY,
-    ORIGIN: ["https://fakenews-eta.vercel.app/"],
+    ORIGIN: ["https://fakenews-eta.vercel.app"],
 };
